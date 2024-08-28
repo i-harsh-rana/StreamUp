8e5b8a87c9022982290067c3d7b38cad458fc466
Harsh Rana <harshrana8454@gmail.com>
2024-08-28 13:18:54 +0530

    done with dashboard controller

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import playlistRouter from './routes/playlist.routes.js'
 import tweetRouter from './routes/tweet.routes.js'
 import commentRouter from './routes/comment.routes.js'
 import likeRouter from './routes/like.routes.js'
+import dashboardRouter from './routes/dashboard.routes.js'
 
 app.use('/api/v1/user', userRouter)
 
@@ -36,6 +37,8 @@ app.use('/api/v1/tweet', tweetRouter)
 app.use('/api/v1/comment', commentRouter)
 
 app.use('/api/v1/like', likeRouter)
+
+app.use('/api/v1/dashboard', dashboardRouter)
 
 
 export default app; 
--- src/controllers/dashborad.controller.js
+++ src/controllers/dashborad.controller.js
@@ -7,11 +7,59 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async(req, res)=>{
+    const userId = req.user
+
+    if (!userId) {
+        throw new ApiError(404, "Please login");
+    }
+
+    const findVideosOfChannel = await Video.find({owner: userId._id}).select('-videoFile -description -owner')
+
+    if(findVideosOfChannel.length === 0){
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, null, "Channel doesn't have any video")
+        )
+    }
+
+    const subscribersCount = await Subscription.countDocuments({channel: userId._id})
+
+    const videoId = findVideosOfChannel.map(video => video._id)
+
+    const videoLikes = await Like.countDocuments({video: {$in: videoId}})
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, {userId, findVideosOfChannel, subscribersCount, videoLikes}, "Channel stats fetched successfully")
+    )
+
 
 })
 
 const getChannelVideos = asyncHandler(async(req, res)=>{
+    const user = req.user
 
+    if (!user) {
+        throw new ApiError(404, "Please login");
+    }
+
+    const findAllVideoOfCurrentChannel = await Video.find({owner: user._id})
+
+    if(findAllVideoOfCurrentChannel.length === 0){
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, null, "Channel doesn't have any video")
+        )
+    }
+
+    return res 
+    .status(200)
+    .json(
+        new ApiResponse(200, findAllVideoOfCurrentChannel, "All Video Fetdched")
+    )
 })
 
 export {
