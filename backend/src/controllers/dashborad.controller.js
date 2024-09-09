f63e6401e9d31d210fe842ef81a332eacbe39722
Harsh Rana <harshrana8454@gmail.com>
2024-09-09 15:02:02 +0530

    some adjustments according to frontend

--- backend/src/controllers/dashborad.controller.js
+++ backend/src/controllers/dashborad.controller.js
@@ -5,38 +5,57 @@ import { Subscription } from '../models/subscription.model.js';
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {User} from '../models/user.model.js'
 
-const getChannelStats = asyncHandler(async(req, res)=>{
-    const userId = req.user
 
+const getChannelStats = asyncHandler(async (req, res) => {
+    const userId = req.user;
     if (!userId) {
         throw new ApiError(404, "Please login");
     }
 
-    const findVideosOfChannel = await Video.find({owner: userId._id}).select('-videoFile -description -owner')
+    const user = await User.findById(userId._id)
+        .populate({
+            path: 'watchHistory',
+            select: '_id thumbnail title duration views createdAt',
+            populate: {
+                path: 'owner',
+                select: 'avatar username'
+            }
+        })
+        .select('-password -refreshToken');
 
-    if(findVideosOfChannel.length === 0){
-        return res
-        .status(200)
-        .json(
-            new ApiResponse(200, null, "Channel doesn't have any video")
-        )
+    if (!user) {
+        throw new ApiError(404, "User not found");
     }
 
-    const subscribersCount = await Subscription.countDocuments({channel: userId._id})
+    const findVideosOfChannel = await Video.find({ owner: userId._id })
+        .select('-videoFile -description -owner');
 
-    const videoId = findVideosOfChannel.map(video => video._id)
+    if (findVideosOfChannel.length === 0) {
+        return res
+            .status(200)
+            .json(new ApiResponse(200, null, "Channel doesn't have any video"));
+    }
 
-    const videoLikes = await Like.countDocuments({video: {$in: videoId}})
+    const subscribersCount = await Subscription.countDocuments({ channel: userId._id });
+    const subscribedChannelsCount = await Subscription.countDocuments({ subscriber: userId._id });
+    const videoId = findVideosOfChannel.map(video => video._id);
+    const videoLikes = await Like.countDocuments({ video: { $in: videoId } });
 
     return res
-    .status(200)
-    .json(
-        new ApiResponse(200, {userId, findVideosOfChannel, subscribersCount, videoLikes}, "Channel stats fetched successfully")
-    )
-
-
-})
+        .status(200)
+        .json(new ApiResponse(200, {
+            userId: {
+                ...user.toObject(),
+                watchHistory: user.watchHistory,
+                subscribersCount: subscribersCount,
+                subscribedChannelsCount: subscribedChannelsCount,
+                totalVideoLikes: videoLikes
+            },
+            findVideosOfChannel,
+        }, "Channel stats fetched successfully"));
+});
 
 const getChannelVideos = asyncHandler(async(req, res)=>{
     const user = req.user
